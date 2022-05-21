33eeb86291c2edf3629b5e8ee897fffabbe0d9ed
Hedeus <94357857+Hedeus@users.noreply.github.com>
2022-05-21 09:36:44 +0300

    Базовый класс модели-представления

---
+++ Bachelor/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Markup;
+using System.Xaml;
+
+namespace Bachelor.ViewModels.Base
+{
+    internal abstract class ViewModel : MarkupExtension, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var value_target_service = serviceProvider.GetService(typeof(IProvideValueTarget)) as IProvideValueTarget;
+            var root_object_service = serviceProvider.GetService(typeof(IRootObjectProvider)) as IRootObjectProvider;
+
+            OnInitialized(
+                value_target_service?.TargetObject,
+                value_target_service?.TargetProperty,
+                root_object_service?.RootObject);
+
+            return this;
+        }
+        private WeakReference _TargetRef;
+        private WeakReference _RootRef;
+
+        public object TargetObject => _TargetRef.Target;
+        public object RootObject => _RootRef.Target;
+        protected virtual void OnInitialized(object Target, object Property, object Root)
+        {
+            _TargetRef = new WeakReference(Target);
+            _RootRef = new WeakReference(Root);
+        }
+    }
+}
