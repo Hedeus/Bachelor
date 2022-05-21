0d0d7c9c34ba03fa93cbe9a304f7c3c5a829e021
Hedeus <94357857+Hedeus@users.noreply.github.com>
2022-05-21 10:01:02 +0300

    Добавлена основа для написания команд

---
+++ Bachelor/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Bachelor.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Markup;
+
+namespace Bachelor.ViewModels
+{
+    [MarkupExtensionReturnType(typeof(MainWindowViewModel))]
+    internal class MainWindowViewModel : ViewModel
+    {
+        /*---------------------------------------------Properies------------------------------------------------*/
+        #region Свойства
+
+        #region Title - Заголовок вікна
+        private string _Title = "Бакалаврська робота";
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        } 
+        #endregion
+
+
+        #endregion
+
+    }
+}
