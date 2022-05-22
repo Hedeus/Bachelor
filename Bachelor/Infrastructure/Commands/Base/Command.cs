10b590109e6e92cb19c445cfd2e2fb39054f402d
Hedeus <94357857+Hedeus@users.noreply.github.com>
2022-05-22 09:50:37 +0300

    Переопределены базовые классы команд  для лучшей управляемости и защищенности.

--- Bachelor/Infrastructure/Commands/Base/Command.cs
+++ Bachelor/Infrastructure/Commands/Base/Command.cs
@@ -10,7 +10,26 @@ namespace Bachelor.Infrastructure.Commands.Base
             add => CommandManager.RequerySuggested += value;
             remove => CommandManager.RequerySuggested -= value;
         }
-        public abstract bool CanExecute(object parameter);
-        public abstract void Execute(object parameter);
+        private bool _Executable;
+        public bool Executable
+        {
+            get => _Executable;
+            set
+            {
+                if (_Executable == value) return;
+                _Executable = value;
+                CommandManager.InvalidateRequerySuggested();
+            }
+        }
+
+        bool ICommand.CanExecute(object parameter) => _Executable && CanExecute(parameter);
+        void ICommand.Execute(object parameter)
+        {
+            if (CanExecute(parameter))
+                Execute(parameter);
+        }
+
+        protected virtual bool CanExecute(object parameter) => true;
+        protected abstract void Execute(object parameter);
     }
 }
