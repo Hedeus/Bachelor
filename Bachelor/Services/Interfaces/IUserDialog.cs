ece352ffb0985054e3f1c91a192adf5ea6c8406e
Hedeus <94357857+Hedeus@users.noreply.github.com>
2022-05-22 11:02:43 +0300

    Диалог с пользователем по выбору файла

--- Bachelor/Services/Interfaces/IUserDialog.cs
+++ Bachelor/Services/Interfaces/IUserDialog.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace Bachelor.Services.Interfaces
 {
     internal interface IUserDialog
     {
+        bool OpenFile(string Title, out string SelectedFile, string Filter = "Все файлы (*.*)|*.*");
+
+        bool OpenFiles(string Title, out IEnumerable<string> SelectedFiles, string Filter = "Все файлы (*.*)|*.*");
     }
 }
